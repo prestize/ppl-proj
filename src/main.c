239210a4fe8b4ce7d6dbd76dcc4ed99882b1e339
OrlannArgarin <73703060+OrlannArgarin@users.noreply.github.com>
2022-12-09 18:56:02 +0800

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "include/lexer.h"
+
+int main(int argc, char *argv[])
+{
+    lexer_T *lexer = init_lexer(
+        "name = \"john\"\n"
+        "display(name)\n");
+
+    token_T *token = (void *)0;
+
+    while ((token = lexer_get_next_token(lexer)) != (void *)0)
+    {
+        printf("TOKEN(%d, %s)\n", token->type, token->value);
+    }
+
+    return 0;
+}
