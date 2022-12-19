391645a361fd8c182865ee004bded305a606aa83
Shiela Bacud <shielamaebacuds@gmail.com>
2022-12-19 13:01:17 +0800

    prevent program from reading other filetypes

--- src/io.c
+++ src/io.c
@@ -1,30 +1,45 @@
 #include "include/io.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char *get_file_contents(const char *filepath)
 {
     char *buffer = 0;
     long length;
+    FILE *f;
 
-    FILE *f = fopen(filepath, "r");
-
-    if (f)
+    int length_f = strlen(filepath);
+    if (filepath[length_f-1]=='z'  && filepath[length_f-2]=='d' && filepath[length_f-3]=='i'  && filepath[length_f-4]=='k' && filepath[length_f-5]=='.' )
     {
-        fseek(f, 0, SEEK_END);
-        length = ftell(f);
-        fseek(f, 0, SEEK_SET);
+        f = fopen(filepath, "r");
+        if (f)
+        {
+            fseek(f, 0, SEEK_END);
+            length = ftell(f);
+            fseek(f, 0, SEEK_SET);
 
-        buffer = calloc(length, length);
+            buffer = calloc(length, length);
 
-        if (buffer)
-            fread(buffer, 1, length, f);
+            if (buffer)
+                fread(buffer, 1, length, f);
 
-        fclose(f);
+            fclose(f);
 
-        return buffer;
+            return buffer;
+        }
+
+        printf("Error reading file %s\n", filepath);
+        exit(2);
     }
 
-    printf("Error reading file %s\n", filepath);
-    exit(2);
+    else
+    {
+        printf("Filetype not supported.\n");
+        exit(2);
+    }
+
+   
+
+    
 }
